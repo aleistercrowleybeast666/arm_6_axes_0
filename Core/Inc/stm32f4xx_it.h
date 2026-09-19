ce0f69788590a21fa71ef7ab60327546d48ffb7b
acb6 <cxylhtgzs@gmail.com>
2026-09-19 17:26:38 +0800

    修改了cubemx的TIM配置

--- Core/Inc/stm32f4xx_it.h
+++ Core/Inc/stm32f4xx_it.h
@@ -56,9 +56,9 @@ void EXTI0_IRQHandler(void);
 void EXTI4_IRQHandler(void);
 void CAN1_RX0_IRQHandler(void);
 void CAN1_SCE_IRQHandler(void);
+void TIM1_UP_TIM10_IRQHandler(void);
 void USART1_IRQHandler(void);
 void SDIO_IRQHandler(void);
-void TIM6_DAC_IRQHandler(void);
 void DMA2_Stream3_IRQHandler(void);
 void DMA2_Stream6_IRQHandler(void);
 /* USER CODE BEGIN EFP */
